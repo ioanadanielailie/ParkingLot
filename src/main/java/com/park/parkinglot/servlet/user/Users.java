5999dbc7a23941f9802e697495a768d69dffbe23
ioanadanielailie <ioanadaniela71200@gmail.com>
2021-12-13 21:33:06 +0200

    Adding other elements

--- src/main/java/com/park/parkinglot/servlet/user/Users.java
+++ src/main/java/com/park/parkinglot/servlet/user/Users.java
@@ -6,10 +6,13 @@
 package com.park.parkinglot.servlet.user;
 
 import com.park.parkinglot.common.UserDetails;
+import com.park.parkinglot.ejb.InvoiceBean;
 import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
@@ -37,17 +40,11 @@ public class Users extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Inject
-    private UserBean userBean;
+    UserBean userBean;
+    
+    @Inject
+    InvoiceBean invoiceBean;
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -55,21 +52,28 @@ public class Users extends HttpServlet {
 
         List<UserDetails> users = userBean.getAllUsers();
         request.setAttribute("users", users);
+        
+        if(!invoiceBean.getUserIds().isEmpty()){
+            Collection<String>usernames=userBean.findUsernames(invoiceBean.getUserIds());
+            request.setAttribute("invoices", usernames);
+        }
 
         request.getRequestDispatcher("/WEB-INF/pages/user/users.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String[] userIdsAsString=request.getParameterValues("user_ids");
+        if(userIdsAsString !=null){
+            Set<Integer> userIds=new HashSet<>();
+            for(String userIdAsString : userIdsAsString){
+                userIds.add(Integer.parseInt(userIdAsString));
+            }
+  
+            invoiceBean.getUserIds().addAll(userIds);
+        }
+         response.sendRedirect(request.getContextPath()+"/Users");
     }
 
     /**
--- src/main/webapp/WEB-INF/pages/user/users.jsp
+++ src/main/webapp/WEB-INF/pages/user/users.jsp
@@ -6,24 +6,36 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
-<%@ taglib prefix="u" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="u" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <t:pageTemplate pageTitle="Users">
     <h1>Users</h1>
-    <c:if test="${pageContext.request.isUserInRole('AdminRole')}">
+    <form method="POST" action="${pageContext.request.contextPath}/Users">
+    <u:if test="${pageContext.request.isUserInRole('AdminRole')}">
         <a class="btn btn-primary btn-lg" href="${pageContext.request.contextPath}/Users/Create" role="button">Add User</a>
-    </c:if>
+        <button class="btn btn-secondary" type="submit">Invoice</button>
+    </u:if>
     <u:forEach var="user" items="${users}" varStatus="status">
         <div class="row">
+            <div class="col-md">
+                <input type="checkbox" name="user_ids" value="${user.id}"/>
+            </div>
             <div class="col-md-4">
                 ${user.username}
             </div>
             <div class="col-md-4">
                 ${user.email}
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 ${user.position}
             </div>
         </div>
     </u:forEach>
+    </form>
+
+    Invoice for:
+    <u:forEach var="username" items="${invoices}" varStatus="status">
+        ${username}
+    </u:forEach>
+        
 </t:pageTemplate>
